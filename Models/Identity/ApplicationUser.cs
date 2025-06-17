73010f48fff16f9ce92ff6a747224982e862bd0f
Franklin William <ui.develloper@gmail.com>
2025-06-17 20:43:54 -0300

    Teste de compilação

--- Models/Identity/ApplicationUser.cs
+++ Models/Identity/ApplicationUser.cs
@@ -12,3 +12,4 @@ namespace RyujinBites.Models.Identity
 
     }
 }
+ 
---
+++ Models/Lanchonete/HomeProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations; // Usado para atributos como [Display], [Required] se você os adicionar.
+
+namespace RyujinBites.Models.Lanchonete // <--- GARANTA QUE ESTE NAMESPACE ESTÁ CORRETO
+{
+    public class HomeProductViewModel
+    {
+        public int Id { get; set; } // ID do produto
+        public string Nome { get; set; } = string.Empty; // Nome do produto
+        public string Descricao { get; set; } = string.Empty; // Descrição curta
+        public decimal Preco { get; set; } // Preço do produto
+        public string ImagemUrl { get; set; } = string.Empty; // URL da imagem
+    }
+}
